3ff65b57db5da0b7bcbd0d3191d0d406d5bd0781
Patryk Mielech <patryk.mielech@protonmail.com>
2024-04-16 19:32:37 +0200

    Added c/cpp config, modified mason config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,8 @@ require("lazy").setup({
 		},
 		-- import any extras modules here
 		{ import = "lazyvim.plugins.extras.lang.python" },
+		{ import = "lazyvim.plugins.extras.lang.clangd" },
+		{ import = "lazyvim.plugins.extras.lang.cmake" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.dap.core" },
